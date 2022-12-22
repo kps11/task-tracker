a13794bba9c853e03b48d0b9c0a7ba9c366c4559
kps11 <kpsenapati6@gmail.com>
2022-12-22 20:33:32 +0530

    task tracker with vue js

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button  @click="onClick()" :style="{ background : color}" class="btn"> {{ name }}</button>
+</template>
+
+<script>
+    export default{
+        name : "Button",
+        props:{
+            name: String,
+            color: String
+        },
+        methods:{
+            onClick() {
+                this.$emit('btn-click')
+            }
+        }
+
+    }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header>
+        <h1>
+            {{ title }}
+        </h1>
+        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')"
+        :name= "showAddTask ? 'Close' : 'Add Task'" 
+        :color="showAddTask ? 'red' : 'green'"/>
+        <!-- <Button name="Update Task" color="Blue"/>
+        <Button name="Delete Task" color="Red"/> -->
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue"
+export default {
+    name : "Header",
+    // props:['title'],
+    props: {
+        title:{
+            type : String,
+            default: 'Hello World'
+        },
+        showAddTask :{
+            type: Boolean
+        }
+    },
+    components:{
+        Button
+    },
+    computed: {
+        homePage(){
+            if(this.$route.path === "/"){
+                return true
+            }else{
+                return false
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-bottom: 2em;
+    }
+</style>
